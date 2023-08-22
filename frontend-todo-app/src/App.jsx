8eb44b38d47ee7e6e731846a9300f7a10dbb8109
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-23 00:54:39 +0530

    setup

---
+++ frontend-todo-app/src/App.jsx
@@ -0,0 +1,23 @@
+
+var todos=[{
+  title:"h1",
+  description:"hemlo",
+  id:1
+}  ,{title:"hi",
+description:"hemlo",
+id:1
+},  {title:"h1",
+description:"hemlo",
+id:1
+}]
+function App() {
+ 
+ 
+  return (
+    <>
+      <h1>{todos[1].title}</h1>
+    </>
+  )
+}
+
+export default App
